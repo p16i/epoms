837b43678fa8632f95cf841a2aa874dae1e75873
heytitle <pat.chormai@gmail.com>
2015-10-19 16:11:01 +0200

    Pagerank Evaluation

--- scripts/json-name-graph.py
+++ scripts/json-name-graph.py
@@ -5,31 +5,54 @@ from os import listdir
 from os.path import isfile, join
 from concurrent import futures
 
-from epoms.db import *
-from epoms.entity_extract import EntityExtract
+import sys
+
 
 import json
 
-en = EntityExtract()
-news = (News().select().limit(10))
 
+filename = sys.argv[1]
+csv      = sys.argv[2]
+
+lines = open( filename, 'r').readlines()
+
+ranks = dict()
+for l in open( csv, 'r').readlines():
+    ( name, rank ) = l.strip().split(' ')
+    ranks[name] = float(rank)
+
+
+graph = dict()
+
+for l in lines:
+    ( src, dest ) = l.strip().split(' ')
+    if( src in graph.keys() and dest not in graph[src] ):
+        graph[src].append(dest)
+    else:
+        graph[src] = [ dest ]
+
+def find_subtree( name, depth ):
+    if( depth == 0 ):
+        return []
+    else:
+        subtree = [ { "name": name, "nodes": graph[name] } ]
+        for t in graph[name]:
+            subtree = subtree + find_subtree( t, depth - 1 )
+
+        return subtree
+
+tree = find_subtree( 'Philips', 2 )
 
 graph = {
     "nodes" : [],
     "links": []
 }
-
 edges = []
 nodes = []
 
-for n in news:
-    names = en.extract_name( n.content )
-    keys = names.keys()
-
-    for i in range(len(keys)):
-        for j in range(len(keys)):
-            if( i != j ):
-                edges.append( ( keys[i], keys[j] ) )
+for n in tree:
+    for d in n['nodes']:
+        edges.append( ( n['name'], d ) )
 
 for e in edges:
     l = [0,0]
@@ -38,14 +61,17 @@ for e in edges:
             nodes.append(e[i])
 
         l[i] = nodes.index(e[i])
-    graph['links'].append({
-        'source': l[0],
-        'target': l[1]
-    })
+
+        graph['links'].append({
+            'source': l[0],
+            'target': l[1]
+        })
 
 for n in nodes:
+    # TODO: Add pagerank here
     node = {
-        'name': n
+        'name': n,
+        'rank': ranks[n]
     }
 
 
